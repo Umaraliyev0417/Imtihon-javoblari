596068ac3584ba81a671c2a167ef475446affa82
Umaraliyev0417 <umaraliyev0417@gmail.com>
2023-12-17 15:41:42 +0500

    Javoblar.

---
+++ main.py
@@ -0,0 +1,29 @@
+#1. Tushirib qoldirilgan sonni topuvchi funksiya tuzing!
+
+# list = [1, 2, 9, 5, 3, 8, 10, 4, 6]
+# n = range(1, 10 + 1)
+#
+# for x in n:
+#     if x not in list:
+#         print(x)
+
+
+#2. Bitta moshina 5 ta odam olishi mumkin, kiritilgan odamlar soniga qarab moshina nechta kerakligini toping!
+
+# m = int(input("Odamlar soni: "))
+# j = 5
+# n = m // j + 1
+# print(n)
+
+
+
+
+# 3. listda sonlar berilgan, ularni foizini qaytarish!
+#
+# list = [10, 13, 9, 3, 5]
+# b = 100
+# f = int(input("Foizni kiriting: "))
+#
+# for x in list:
+#     m = x / b * f
+#     print(m)
